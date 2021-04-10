f2266a442833211e17c92272f46f5bbd2f1f3435
cturner8 <cammy_smfc@live.com>
2021-04-10 17:03:03 +0100

    added middleware and product routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import "colors";
 
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+
+import productRoutes from "./routes/product.routes.js";
+import { notFound, errorHandler } from "./middleware/error.middleware.js";
 
 dotenv.config();
 connectDB();
@@ -17,14 +19,10 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/product/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(
   PORT,
